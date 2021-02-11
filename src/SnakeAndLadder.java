f90aa4cb06dbca01c664154bc16ed6bfce41e5c7
ketankotian <krkkotian@gmail.ocm>
2021-02-11 22:46:54 +0530

    Game starts at position 0

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,7 +1,11 @@
 
 public class SnakeAndLadder {
+	public static final int StartPos = 0;
+	public static final int WinPos = 100;
 
 	public static void main(String[] args) {
+
+		System.out.println("Game starts at position : " + StartPos);
 
 	}
 
