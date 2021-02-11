cf411b1c3701cb2496b7c508c4211f897a14a64a
ketankotian <krkkotian@gmail.ocm>
2021-02-12 01:03:06 +0530

    Ensure Player wins at 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -16,11 +16,21 @@ public class SnakeAndLadder {
 			int option = r.nextInt(3);
 			// option-0=ladder,1=snake,2=no-play
 
+			if (option == 0) {
+				if (position == end) {
+					System.out.println("Player Wins !!! ");
+					break;
+				} else if (position + dice > 100) {
+					position = position - dice;
+				}
+			}
+
 			switch (option) {
 			case 0:
 				position = position + dice;
 				System.out.println("Player climbs ladder : " + position);
 				break;
+
 			case 1:
 				if (position >= 0) {
 					position = position - dice;
