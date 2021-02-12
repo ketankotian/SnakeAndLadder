6ef04f1200780863fdf9dc4e3443bb92cc807d12
ketankotian <krkkotian@gmail.ocm>
2021-02-13 00:44:03 +0530

    Added Player2

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -10,46 +10,86 @@ public class SnakeAndLadder {
 		int dice = r.nextInt(6) + 1;
 		System.out.println("Dice value : " + dice);
 
-		int position = 0, count = 0;
+		int player1Position = 0, player2Position = 0, count = 0;
+		boolean player1 = true, player2 = false;
 
-		while (position <= 100) {
+		while (player1Position <= 100 && player2Position <= 100) {
 			int option = r.nextInt(3);
 			// option-0=ladder,1=snake,2=no-play
 
 			if (option == 0) {
-				if (position == end) {
-					System.out.println("Player Wins !!! ");
+				if (player1 && player1Position == end) {
+					System.out.println("Player1 Wins !!! ");
+					player1 = true;
+					player2 = false;
 					break;
-				} else if (position + dice > 100) {
-					position = position - dice;
+
+				} else if (player2 && player2Position == end) {
+					System.out.println("Player2 Wins !!! ");
+					player2 = true;
+					player1 = false;
+					break;
+
+				} else if (player1Position + dice > 100) {
+					player1Position = player1Position - dice;
+				} else if (player2Position + dice > 100) {
+					player2Position = player2Position - dice;
 				}
 			}
 
 			switch (option) {
 			case 0:
-				position = position + dice;
-				System.out.println("Player climbs ladder : " + position);
-				count++;
+				if (player1) {
+					player1Position = player1Position + dice;
+					System.out.println("Player1 climbs ladder : " + player1Position);
+					player1 = true;
+					player2 = false;
+					count++;
+				} else {
+					player2Position = player2Position + dice;
+					System.out.println("Player2 climbs ladder : " + player1Position);
+					player2 = true;
+					player1 = false;
+					count++;
+
+				}
 				break;
 
 			case 1:
-				position = position - dice;
-				if (position >= 0) {
-					System.out.println("Snake pulls the player : " + position);
+				if (player1) {
+					player1Position = player1Position - dice;
+					if (player1Position >= 0) {
+						System.out.println("Snake pulls the player1 : " + player1Position);
+					} else {
+						player1Position = 0;
+						System.out.println("Snake pulls the player1 : " + player1Position);
+					}
+					player1 = true;
+					player2 = false;
+
+					count++;
 				} else {
-					position = 0;
-					System.out.println("Snake pulls the player : " + position);
+					player2Position = player2Position - dice;
+					if (player2Position >= 0) {
+						System.out.println("Snake pulls the player2 : " + player2Position);
+					} else {
+						player1Position = 0;
+						System.out.println("Snake pulls the player2 : " + player2Position);
+					}
+					player2 = true;
+					player1 = false;
+
+					count++;
 				}
-				count++;
 				break;
 			case 2:
-				System.out.println("Player is in no-play : " + position);
+				System.out.println("Player1 is in no-play : " + player1Position);
+				System.out.println("Player2 is in no-play : " + player2Position);
 				count++;
 				break;
 			}
-			System.out.println("count : " + count );
+			System.out.println("count : " + count);
 		}
 
 	}
-
 }
